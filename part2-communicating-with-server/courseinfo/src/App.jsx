566429bc2ede978f9dabd97848260b4a9cf3ff0d
vjckmaciej <macwojcik20@gmail.com>
2025-06-16 22:29:36 +0200

    Part 2 - exercise 2.4 solution

--- part2-communicating-with-server/courseinfo/src/App.jsx
+++ part2-communicating-with-server/courseinfo/src/App.jsx
@@ -15,6 +15,7 @@ const Course = ({ course }) => {
     <>
       <Header course={course} />
       <Content course={course} />
+      <Total course={course} />
     </>
   );
 };
@@ -38,41 +39,58 @@ const Total = ({ course }) => (
     </strong>
   </p>
 );
-//   {
-//   return (
-//     <>
-//       {/* <p>Number of exercises {course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises} </p> */}
-//     </>
-//   )
-// }
 
 const App = () => {
-  const course = {
-    id: 1,
-    name: "Half Stack application development",
-    parts: [
-      {
-        name: "Fundamentals of React",
-        exercises: 10,
-        id: 1,
-      },
-      {
-        name: "Using props to pass data",
-        exercises: 7,
-        id: 2,
-      },
-      {
-        name: "State of a component",
-        exercises: 14,
-        id: 3,
-      },
-    ],
-  };
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
 
   return (
     <>
-      <Course course={course} />
-      <Total course={course} />
+      {courses.map(course => (
+        <Course key={course.id} course={course} />
+      ))}
     </>
   );
 };
