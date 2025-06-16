27c9b7f618aa1bbd3d18d2d66e816a91b87cd01a
vjckmaciej <macwojcik20@gmail.com>
2025-06-17 00:27:25 +0200

    Part 2 - exercise 2.6 solution

---
+++ part2-communicating-with-server/phonebook/src/App.jsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+
+const App = () => {
+  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [newName, setNewName] = useState("");
+
+  const addNumber = (event) => {
+    event.preventDefault();
+    const newPerson = { name: newName };
+    setPersons(persons.concat(newPerson));
+    setNewName("");
+  };
+
+  const handleNumberChange = (event) => {
+    setNewName(event.target.value);
+  };
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addNumber}>
+        <div>
+          name: <input value={newName} onChange={handleNumberChange} />
+        </div>
+        <button type="submit">add</button>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map((person) => (
+        <p key={person.name}>{person.name}</p>
+      ))}
+    </div>
+  );
+};
+
+export default App;
