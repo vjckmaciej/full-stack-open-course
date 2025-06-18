0d583f84f900d30115bf5e67b91190fbdf193acd
vjckmaciej <macwojcik20@gmail.com>
2025-06-18 02:06:01 +0200

    Part 2 - exercise 2.15 solution

--- part2-communicating-with-server/phonebook/src/App.jsx
+++ part2-communicating-with-server/phonebook/src/App.jsx
@@ -21,9 +21,25 @@ const App = () => {
     event.preventDefault();
     const newPerson = { name: newName, number: newNumber };
     const exists = persons.some((person) => person.name === newName);
+    const existingPerson = persons.find((person) => person.name === newName);
+
     if (exists) {
-      alert(newName + " is already added to phonebook");
-      return;
+      if (
+        window.confirm(
+          newName +
+            " is already added to phonebook, replace the old number with a new one?"
+        )
+      ) {
+        phonebookService
+          .update(existingPerson.id, newPerson)
+          .then((returnedPerson) => {
+            setPersons(
+              persons.map((person) =>
+                person.id === existingPerson.id ? returnedPerson : person
+              )
+            );
+          });
+      }
     } else {
       phonebookService.create(newPerson).then((returnedNumber) => {
         setPersons(persons.concat(returnedNumber));
